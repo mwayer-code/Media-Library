a3f241da30348c611ac7b8897979ec7dc2b63c75
mwayer-code <mwayer@my.wctc.edu>
2024-03-23 17:02:32 -0500

    [1.11] Create Menu to Add a Movie and Display All Movies

--- Program.cs
+++ Program.cs
@@ -11,4 +11,35 @@ string scrubbedFile = FileScrubber.ScrubMovies("movies.csv");
 logger.Info(scrubbedFile);
 MovieFile movieFile = new MovieFile(scrubbedFile);
 
+string choice = "";
+while (choice != "q")
+{
+    Console.WriteLine("1) Add Movie");
+    Console.WriteLine("2) Display All Movies");
+    Console.WriteLine("q) Quit");
+    choice = Console.ReadLine();
+
+    if (choice == "1")
+    {
+        Movie movie = new Movie();
+        Console.WriteLine("Enter movie title");
+        movie.title = Console.ReadLine();
+        Console.WriteLine("Enter movie genres (action|comedy|etc)");
+        string genres = Console.ReadLine();
+        movie.genres = genres.Split('|').ToList();
+        Console.WriteLine("Enter movie director");
+        movie.director = Console.ReadLine();
+        Console.WriteLine("Enter movie run time (h:m:s)");
+        movie.runningTime = TimeSpan.Parse(Console.ReadLine());
+        movieFile.AddMovie(movie);
+    }
+    else if (choice == "2")
+    {
+        foreach (Movie m in movieFile.Movies)
+        {
+            Console.WriteLine(m.Display());
+        }
+    }
+}
+
 logger.Info("Program ended");
